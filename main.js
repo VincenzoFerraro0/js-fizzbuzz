548dbc44679229211722beab12017e6924eb4a6d
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-10 13:41:07 +0100

    task b.3 completed

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ for (let i = 1; i <= 100; i++){
         console.log("FizzBuzz")
     }else if (i % 3 === 0){
         console.log("Fizz")
+    }else if (i % 5 === 0) {
+        console.log ("Buzz")
     }
 }
    
